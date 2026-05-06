cc0a9bd607ddcceaeb77553433b4b1f4b47eadb8
harilalaherdy-a11y <harilalaherdy@gmail.com>
2026-05-06 12:53:50 +0300

    Initial commit - React + Vite setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>🏆 Esport Madagascar Pro</h1>
+      <p>Le site est en cours de déploiement...</p>
+    </div>
+  )
+}
+
+export default App
